ec9642688d871307b1ba5176f2cf22855df98617
barrett4467 <barrett4467@gmail.com>
2021-06-04 07:32:18 -0600

    connected to Mongodb

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 var express = require('Express');
 var app = express();
-app.use(express.static("public"));
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
 
+dotenv.config();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+//Database connection
+mongoose.set("useFindAndModify", false);
+mongoose.connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true}, ()=>{
+        console.log("Connected to Mongodb!");
+         //server
+    app.listen(3000, () => 
+    console.log("Server up and running on port 3000")
+    );
+    })
 app.set("view engine", "ejs");
 
 app.get('/', function(req, res){
     res.render("todo.ejs");
  });
  app.post('/', function(req, res){
-    res.send('POST route on index.');
+    console.log(req.body);
  });
  app.put('/', function(req, res){
      res.send('POST route on index.');
@@ -17,7 +32,3 @@ app.get('/', function(req, res){
      res.send('POST route on index.');
  });
 
- //server
-app.listen(3000, () => 
-    console.log("Server up and running on port 3000")
-);
