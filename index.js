32df5707f3c16396a651b7778fbec2ef392039b9
barrett4467 <barrett4467@gmail.com>
2021-06-04 07:00:26 -0600

    Error with mime type was due to extra slash

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('Express');
 var app = express();
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
 
 app.get('/', function(req, res){
