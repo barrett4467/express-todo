7b39db2f4d7e35654f939af1c00ea42c55212675
barrett4467 <barrett4467@gmail.com>
2021-06-10 20:14:40 -0600

    Added update and delete methods

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/', function(req, res){
     TodoTask.find({}, (err, tasks) => {
         res.render("todo.ejs", { todoTasks: tasks });
     })});
- //POST Method
+//POST Method
  app.post('/', async (req, res) => {
      try {
         const todoTask = new TodoTask({
@@ -52,10 +52,31 @@ app.get('/', function(req, res){
      }
  });
 
- app.put('/', function(req, res){
-     res.send('POST route on index.');
- });
- app.delete('/', function(req, res){
-     res.send('POST route on index.');
- });
+ //UPDATE Method
+app.route("/edit/:id")
+.get((req, res) => {
+    const id = req.params.id;
+    TodoTask.find({}, (err, tasks) => {
+        res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id
+        });
+    });
+})
+.post((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
+    });
+});
+//End of UPDATE method
+
+//DELETE Method
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
+    });
+});
+//End of DELETE Method
 
